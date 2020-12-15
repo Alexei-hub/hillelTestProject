383e73d0ad3ca7e528fb140004127e122f5494a7
Alexei <alexeym@pkwteile.de>
2020-12-15 17:42:54 +0200

    commit allure

--- hillelTestProject/src/test/java/ForTest.java
+++ hillelTestProject/src/test/java/ForTest.java
@@ -1,26 +1,13 @@
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 public class ForTest {
 
 
-    @Epic(value = "Математика")
-    @Feature(value = "Простые математические операции")
-    @Story(value = "Сложение")
     @Test
-    @Description(value = "Тест в классе ForTest")
+    @Owner(value = "Maks")
     public void groupTestOne() {
         new MainClass().testMethod(8);
     }
 
-    @Epic(value = "Математика")
-    @Feature(value = "Простые математические операции")
-    @Story(value = "Вычитание")
-    @Test
-    public void groupSubTestOne() {
-        new MainClass().testMethod(6);
-    }
 }
 
